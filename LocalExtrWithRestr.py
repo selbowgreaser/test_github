0d04fabcc7495833bc3945a448d429b9ee5fd1c7
selbowgreaser <selbowgreaser@gmail.com>
2022-02-15 12:31:33 +0300

    <---описание изменений--->

--- LocalExtrWithRestr.py
+++ LocalExtrWithRestr.py
@@ -1,1 +1,21 @@
-print('che to tam')
+import re
+
+
+vars = input().split()
+func_str = input()
+
+if re.findall(f'[^{vars[0], vars[1]}]', re.(r'[\*\+]', 'x*y+10')):
+    print('Вводи новые данные')
+
+func = eval(func_str)
+
+print(func)
+
+
+
+# Введите названия переменных:  x y
+# Введите функцию f(x,y,z)=: y*(x**2)+x*(y**3) – x*y
+# Есть ли ограничения ? 1-да/ 0 – нет : 1
+# Введите допустимые интервалы по x: -10 10
+# Введите ограничения по y:  -1 1
+
